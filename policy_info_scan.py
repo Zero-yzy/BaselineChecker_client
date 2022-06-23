8f916645a00b3fd0c94b5be9a7dd06b9e73846d0
fox12138 <xyxfromxina6@gmail.com>
2022-06-23 10:34:53 +0800

    策略扫描

--- policy_info_scan.py
+++ policy_info_scan.py
@@ -17,19 +17,20 @@ def is_admin():
 
 
 def policy_check(task_id, bsaename):
+    result = {}
+    # 编辑URL
+    result["url"] = "/policyChecks/insert"
+    # 组装发送的类型
+    now = datetime.datetime.now()
+    time = now.strftime("%Y-%m-%d %H:%M:%S")
+    result['finish_time'] = time
+
     if is_admin():
-        result = {}
-        # 编辑URL
-        result["url"] = "/policyChecks/insert"
         # 获取基本信息
         data = get_policy_info(task_id, bsaename)
-        # 组装发送的类型
-        now = datetime.datetime.now()
-        time = now.strftime("%Y-%m-%d %H:%M:%S")
-        result['finish_time'] = time
         result['send_data'] = data
-        # print(result)
-        return result
-
     else:
         ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
+
+    print(result)
+    return result
